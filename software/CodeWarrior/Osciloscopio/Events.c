015d03ef7c183410e2bc40af7c8b1dde603eaf10
Jacobelmenor <55703940+Jacobelmenor@users.noreply.github.com>
2019-11-04 08:24:47 -0400

    Update Events.c

--- software/CodeWarrior/Osciloscopio/Events.c
+++ software/CodeWarrior/Osciloscopio/Events.c
@@ -1,168 +1,141 @@
 /* ###################################################################
-**     Filename    : Events.c
+**     Filename    : main.c
 **     Project     : EC3882-G03
 **     Processor   : MC9S08QE128CLK
-**     Component   : Events
-**     Version     : Driver 01.02
+**     Version     : Driver 01.12
 **     Compiler    : CodeWarrior HCS08 C Compiler
 **     Date/Time   : 2019-09-30, 13:34, # CodeGen: 0
 **     Abstract    :
-**         This is user's event module.
-**         Put your event handler code here.
+**         Main module.
+**         This module contains user's application code.
 **     Settings    :
 **     Contents    :
 **         No public methods
 **
 ** ###################################################################*/
 /*!
-** @file Events.c
-** @version 01.02
+** @file main.c
+** @version 01.12
 ** @brief
-**         This is user's event module.
-**         Put your event handler code here.
+**         Main module.
+**         This module contains user's application code.
 */         
 /*!
-**  @addtogroup Events_module Events module documentation
+**  @addtogroup main_module main module documentation
 **  @{
 */         
-/* MODULE Events */
+/* MODULE main */
 
 
+/* Including needed modules to compile this module/procedure */
 #include "Cpu.h"
 #include "Events.h"
+#include "TI1.h"
+#include "AS1.h"
+#include "AD1.h"
+#include "D1.h"
+#include "D2.h"
+#include "boton.h"
+#include "boton2.h"
+#include "Bit1.h"
+/* Include shared modules, which are used for whole project */
+#include "PE_Types.h"
+#include "PE_Error.h"
+#include "PE_Const.h"
+#include "IO_Map.h"
+
+
 
 /* User includes (#include below this line is not maintained by Processor Expert) */
 
-/*
-** ===================================================================
-**     Event       :  TI1_OnInterrupt (module Events)
-**
-**     Component   :  TI1 [TimerInt]
-**     Description :
-**         When a timer interrupt occurs this event is called (only
-**         when the component is enabled - <Enable> and the events are
-**         enabled - <EnableEvent>). This event is enabled only if a
-**         <interrupt service/event> is enabled.
-**     Parameters  : None
-**     Returns     : Nothing
-** ===================================================================
-*/
-extern int medir;
-int Z = 0;
-extern unsigned char D1;
-void TI1_OnInterrupt(void)
-{
-  /* Write your code here ... */
+void empaquetar(unsigned char b[], unsigned char valor[] , unsigned char valor2[]){
+	b[0] = 0b11111111 | b[0];// encabezado: ADUANA CETIE
 	
-	 medir = 1;
-	 if (Z == 5){
-		 Z = 0;
-		 if (D1 == 0){
-			 D1 = 1;
-			 
-		 } else {
-			 D1 = 0;
-			 
-		 }
-	 }
-	 Z = Z + 1;
+	b[1] |=  valor[0] ;//preguntar como limitar el tamanio del int guardo los 8 bits en valor
+	b[2] |= valor[1] ;
+	
+	b[3] |= valor2[0] ; //preguntar como limitar el tamanio del int guardo los 8 bits en valor
+		b[4] |= valor2[1] ;
+	
+	
+	
+	
 }
+	
 
-/*
-** ===================================================================
-**     Event       :  AD1_OnEnd (module Events)
-**
-**     Component   :  AD1 [ADC]
-**     Description :
-**         This event is called after the measurement (which consists
-**         of <1 or more conversions>) is/are finished.
-**         The event is available only when the <Interrupt
-**         service/event> property is enabled.
-**     Parameters  : None
-**     Returns     : Nothing
-** ===================================================================
-*/
-void AD1_OnEnd(void)
+int medir;
+//int Z = 0;
+unsigned char *olakase;
+void main(void)
 {
-  /* Write your code here ... */
+	//VALO[0] | B[2]		VALO[1]  | B[1]
+//XXXXXXXX			0000XXXX
+	unsigned char valor[2]; // valor[0] = primeros 8 bits del ch1. valor[1] tendra 4 blancos y los 4 restantes de la medicion 
+	unsigned char valor2[2];
+	unsigned char b[5];
+	unsigned int Digital1;
+	unsigned int Digital2;
+  /* Write your local variable definition here */
+  /*** Processor Expert internal initialization. DON'T REMOVE THIS CODE!!! ***/
+  PE_low_level_init();
+  
+  /*** End of Processor Expert internal initialization.                    ***/
+
+  /* Write your code here */
+  
+  /* For example: for(;;) { } */
+  
+	  for(;;) { 
+	
+	  if(medir ==1){
+		  Bit1_NegVal();
+		  valor[0]=0;
+	  	  valor[1]=0;
+		  valor2[0]=0;
+		  valor2[1]=0;
+		  b[0]=0;
+		  b[1]=0;
+		  b[2]=0;
+		  b[3]=0;
+		  b[4]=0;
+	
+	AD1_MeasureChan(TRUE,0); //12 bits
+	AD1_MeasureChan(TRUE,1);
+	AD1_GetChanValue(0, valor); // el valor que arraja el ch1 se guarda en int valor
+	AD1_GetChanValue(1, valor2);	
+	//Digital1 = D1_GetVal();
+	//Digital2 = D2_GetVal();
+	
+			
+	empaquetar(b,valor,valor2);
+	
+	 if(D1_GetVal()){
+			b[1] |= 0b00010000; //asigno el bit del canal digital al b[1]
+		}else b[1] |= 0b00000000;
+		if(D2_GetVal()){
+				b[3] |= 0b00010000; //asigno el bit del canal digital al b[3]
+			} else b[3] |= 0b00000000;
+	//b[1]= 0b00001001;
+	//b[2]= 0b11011101;
+	// Envio de la data
+	
+	AS1_SendBlock(b,5, &olakase); // envio de la data para ser procesada
+	
+	medir =0;
 }
+  }
+  /*** Don't write any code pass this line, or it will be deleted during code generation. ***/
+  /*** RTOS startup code. Macro PEX_RTOS_START is defined by the RTOS component. DON'T MODIFY THIS CODE!!! ***/
+  #ifdef PEX_RTOS_START
+    PEX_RTOS_START();                  /* Startup of the selected RTOS. Macro is defined by the RTOS component. */
+  #endif
+  /*** End of RTOS startup code.  ***/
+  /*** Processor Expert end of main routine. DON'T MODIFY THIS CODE!!! ***/
+  for(;;){}
+  /*** Processor Expert end of main routine. DON'T WRITE CODE BELOW!!! ***/
+} /*** End of main routine. DO NOT MODIFY THIS TEXT!!! ***/
 
-
-/*
-** ===================================================================
-**     Event       :  AS1_OnError (module Events)
-**
-**     Component   :  AS1 [AsynchroSerial]
-**     Description :
-**         This event is called when a channel error (not the error
-**         returned by a given method) occurs. The errors can be read
-**         using <GetError> method.
-**         The event is available only when the <Interrupt
-**         service/event> property is enabled.
-**     Parameters  : None
-**     Returns     : Nothing
-** ===================================================================
-*/
-void  AS1_OnError(void)
-{
-  /* Write your code here ... */
-}
-
-/*
-** ===================================================================
-**     Event       :  AS1_OnRxChar (module Events)
-**
-**     Component   :  AS1 [AsynchroSerial]
-**     Description :
-**         This event is called after a correct character is received.
-**         The event is available only when the <Interrupt
-**         service/event> property is enabled and either the <Receiver>
-**         property is enabled or the <SCI output mode> property (if
-**         supported) is set to Single-wire mode.
-**     Parameters  : None
-**     Returns     : Nothing
-** ===================================================================
-*/
-void  AS1_OnRxChar(void)
-{
-  /* Write your code here ... */
-}
-
-/*
-** ===================================================================
-**     Event       :  AS1_OnTxChar (module Events)
-**
-**     Component   :  AS1 [AsynchroSerial]
-**     Description :
-**         This event is called after a character is transmitted.
-**     Parameters  : None
-**     Returns     : Nothing
-** ===================================================================
-*/
-void  AS1_OnTxChar(void)
-{
-  /* Write your code here ... */
-}
-
-/*
-** ===================================================================
-**     Event       :  AS1_OnFreeTxBuf (module Events)
-**
-**     Component   :  AS1 [AsynchroSerial]
-**     Description :
-**         This event is called after the last character in output
-**         buffer is transmitted.
-**     Parameters  : None
-**     Returns     : Nothing
-** ===================================================================
-*/
-void  AS1_OnFreeTxBuf(void)
-{
-  /* Write your code here ... */
-}
-
-/* END Events */
-
+/* END main */
 /*!
 ** @}
 */
